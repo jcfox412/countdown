b428e632510864a435739e5419eb04a14dc1e3f0
Jamie Fox <jfox@ooyala.com>
2015-04-26 16:06:03 -0700

    Git cleanup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Countdown::Application.config.secret_key_base = '3617149fe23f996a4ad94eb70bd5953ff1dc3ea49c7f55ba354fe9cd2906607220f702a77819bf3f062efd35b1c14d3ae3098c7810226f659e7ea349f717f52a'
+Countdown::Application.config.secret_key_base = '3921de4f1e4f22f04efb23f9064d990b59e23f9ad47eb484c02b4450878d2b91ace2ed46981d39a2583d130b71cf51840ce2fca598beed6aeafd0b193d8c0c8e'
