c43ed1e9079400e4b0f9df6d0766f20a57b2df5b
Jamie Fox <jfox@ooyala.com>
2015-04-26 15:54:37 -0700

    Move app to separate directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Countdown::Application.config.secret_key_base = '3617149fe23f996a4ad94eb70bd5953ff1dc3ea49c7f55ba354fe9cd2906607220f702a77819bf3f062efd35b1c14d3ae3098c7810226f659e7ea349f717f52a'
